422df1c635ec6ab80713abedfb1257868dd52864
Makhmudjon (Max) <100901622+Maximusdev1313@users.noreply.github.com>
2022-11-26 12:24:11 +0000

    changed fields to ChatField

--- commerce/models.py
+++ commerce/models.py
@@ -3,34 +3,34 @@ from django.db import models
 from django.utils.text import slugify
 
 class Product(models.Model):
-    nomi = models.TextField(max_length=100)
-    kilogramm = models.TextField( null=True, blank=True)
-    litri = models.TextField( null=True, blank=True)
-    soni = models.TextField( null=True, blank=True)
-    narx = models.TextField()
-    chegirma_narx = models.TextField(max_length=64, null=True, blank=True)
-    chegirma_foizi = models.TextField(max_length=10, null=True, blank=True)
+    nomi = models.CharField(max_length=100)
+    kilogramm = models.CharField( max_length=100, null=True, blank=True)
+    litri = models.CharField(max_length=100,  null=True, blank=True)
+    soni = models.CharField(max_length=100, null=True, blank=True)
+    narx = models.CharField(max_length=100)
+    chegirma_narx = models.CharField(max_length=64, null=True, blank=True)
+    chegirma_foizi = models.CharField(max_length=10, null=True, blank=True)
     time = models.DateTimeField(auto_now_add=True, )
     mahsulot = models.ForeignKey('Categoriya', on_delete=models.CASCADE, related_name='mahsulot')
 
     def __str__(self):
         return self.nomi
 class ProductRasmi(models.Model):
-    link = models.TextField(max_length=500)
-    title = models.TextField(max_length=2500, null=True)
+    link = models.CharField(max_length=500)
+    title = models.CharField(max_length=2500, null=True)
     rasmlari = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='rasmlari')
     def __str__(self):
         return self.title
 
 
 class Categoriya(models.Model):
-    categoriya_nomi = models.TextField(max_length=200)
+    categoriya_nomi = models.CharField(max_length=200)
     
     def __str__(self):
         return self.categoriya_nomi  
 class CategoriyaRasm(models.Model):
-    link = models.TextField(max_length=500)
-    title = models.TextField(max_length=2500, null=True)
+    link = models.CharField(max_length=500)
+    title = models.CharField(max_length=2500, null=True)
     rasmlar = models.ForeignKey('Categoriya', on_delete=models.CASCADE, related_name='rasmlar')
     def __str__(self):
         return self.title
